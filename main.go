c90e79eb7ab77693fa6c3b9c93a5830c7df0cd0c
Luiz Henrique <luizhenriquembh@gmail.com>
2023-08-29 19:43:44 -0300

    feat[tracks]: add tracks router to main router

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"app/albums"
 	"app/artists"
 	"app/config"
+	"app/tracks"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
@@ -16,6 +17,7 @@ func main() {
 	router := app.Group("/api/")
 	albums.AlbumRoutes(router)
 	artists.ArtistRoutes(router)
+	tracks.TrackRoutes(router)
 
 	app.Run("localhost:42069")
 }
